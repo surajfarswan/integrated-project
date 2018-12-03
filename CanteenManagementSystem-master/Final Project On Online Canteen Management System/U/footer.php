042603502157d34687aa95cddfad7314ffdcdba9
surajfarswan <42631838+surajfarswan@users.noreply.github.com>
2018-12-03 10:53:11 +0530

    Add files via upload
    canteen management system

---
+++ CanteenManagementSystem-master/Final Project On Online Canteen Management System/U/footer.php
@@ -0,0 +1,8 @@
+<div id="footer" style="background-color: Red; height: auto;">
+<table width="100%">
+<tr>
+<td align="center">Developed and Maintained By <b> Deepak Kumar Choudhary 205112033.
+</td>
+</tr>
+</table>
+</div>
